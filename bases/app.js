1c9613d1c1db3dc2554e5212a86b11800b51ebd2
Pelusa-not <agustinlorenzatto@outlook.com>
2025-09-22 17:40:38 -0300

    Empezando el curso de devtalles

---
+++ bases/app.js
@@ -0,0 +1,9 @@
+const msg = 'Hola Mundo';
+const hero = {
+    name: 'IronMan',
+    age: 45
+};
+hero.age = 50;
+console.log(hero);
+export {};
+//# sourceMappingURL=app.js.map
---
+++ bases/app.ts
@@ -0,0 +1,10 @@
+const msg: string = 'Hola Mundo';
+
+const hero = {
+    name: 'IronMan',
+    age: 45
+};
+
+hero.age = 50
+
+console.log(hero);
