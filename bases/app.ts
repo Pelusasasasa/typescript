375913dbcc73b8cdd83635c1077627b044f1e52b
Pelusa-not <agustinlorenzatto@outlook.com>
2025-09-25 10:04:37 -0300

    TErminando nueva seccion

--- bases/app.ts
+++ bases/app.ts
@@ -1,45 +1,32 @@
-(() => {
+// Funciones Básicas
+function sumar( a: number, b:number ): number{
+  return a + b;
+}
 
-    // Tipos
-    const batman: string = 'Bruce';
-    const superman: string = 'Clark';
-  
-    const existe: boolean = false;
-  
-    // Tuplas
-    const parejaHeroes: [string, string] = [batman,superman];
-    const villano: [string,number,boolean] = ['Lex Lutor',5,true];
-  
-    // Arreglos
-    const aliados : string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
-  
-    //Enumeraciones
-    enum power {
-      acuaman = 0,
-      batman = 1,
-      flash = 5,
-      superman = 100,
-    }
-    const fuerzaFlash: power = power.flash;
-    const fuerzaSuperman: power = power.superman;
-    const fuerzaBatman: power = power.batman;
-    const fuerzaAcuaman: power = power.acuaman;
-  
-    // Retorno de funciones
-    function activar_batiseñal(): string{
-      return 'activada';
-    }
-  
-    function pedir_ayuda(): void{
-      console.log('Auxilio!!!');
-    }
-  
-    // Aserciones de Tipo
-    const poder: any = '100';
-    const largoDelPoder:number = (poder as string).length;
-    console.log( largoDelPoder );
-  
-  
-  })()
-  
-  
+const contar = ( heroes: string[] ): number => {
+  return heroes.length;
+}
+const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+contar(superHeroes);
+
+//Parametros por defecto
+const llamarBatman = ( llamar: boolean = true ): void => {
+  if( llamar ){
+    console.log("Batiseñal activada");
+  }
+}
+
+llamarBatman();
+
+// Rest?
+const unirheroes = ( ...personasRest: string[] ): string => {
+  return personasRest.join(", ");
+}
+
+
+// Tipo funcion
+const noHaceNada = ( numero: number, texto: string, booleano: boolean, arreglo: string[] ): void => {}
+
+// Crear el tipo de funcion que acepte la funcion "noHaceNada"
+let noHaceNadaTampoco: (n:number, t:string, b:boolean, a:string[]) => void;
+noHaceNadaTampoco = noHaceNada
