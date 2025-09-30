fa53ef6395254254aa3903cc20732c8c77c603a0
Pelusa-not <agustinlorenzatto@outlook.com>
2025-09-30 16:12:31 -0300

    Terminando e curso

---
+++ express-api/dist/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.status(401).json({
+        ok: false,
+        message: 'No tiene acceso'
+    });
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+//# sourceMappingURL=index.js.map
